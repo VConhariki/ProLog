deb8fee44aae83eeacab3b947ed87cf8979f2352
Vagner de Oliveira Conhariki <43683627+VConhariki@users.noreply.github.com>
2018-11-01 09:26:23 -0300

    Update Maior3.pl

--- Maior3.pl
+++ Maior3.pl
@@ -8,3 +8,4 @@ max(A,B,C):-A=B,A>C,write('Os maiores números são: '),write(A),write(' e '),wr
 max(A,B,C):-A=C,B<C,write('Os maiores números são: '),write(A),write(' e '),write(C).
 max(A,B,C):-B=C,A<C,write('Os maiores números são: '),write(C),write(' e '),write(B).
 max(A,B,C):-A=B,A=C,write('Todos os números são iguais ').
+halt/0. /*Para o programa.*/
