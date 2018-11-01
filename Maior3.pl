7bc0db768959b0a61375b70dff83182c25e76cfc
Vagner de Oliveira Conhariki <43683627+VConhariki@users.noreply.github.com>
2018-11-01 09:17:09 -0300

    Add files via upload

---
+++ Maior3.pl
@@ -0,0 +1,10 @@
+max(A,B,C):-A>B,A>C,write('O maior número é: '),write(A).
+max(A,B,C):-A<B,B>C,write('O maior número é: '),write(B).
+max(A,B,C):-C>B,A<C,write('O maior número é: '),write(C).
+max(A,B,C):-A=B,A<C,write('O maior número é: '),write(C).
+max(A,B,C):-A<B,A=C,write('O maior número é: '),write(B).
+max(A,B,C):-B=C,A>B,write('O maior número é: '),write(A).
+max(A,B,C):-A=B,A>C,write('Os maiores números são: '),write(A),write(' e '),write(B).
+max(A,B,C):-A=C,B<C,write('Os maiores números são: '),write(A),write(' e '),write(C).
+max(A,B,C):-B=C,A<C,write('Os maiores números são: '),write(C),write(' e '),write(B).
+max(A,B,C):-A=B,A=C,write('Todos os números são iguais ').
